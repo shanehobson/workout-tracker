63ade920ccc0d9ddf24adb4d74682d0eb87becf1
Shane Hobson <shanehobson1@gmail.com>
2019-06-01 21:42:17 -0700

    Added layout styles to manage exercises dialog

--- src/app/modals/manage-exercises-dialog/manage-exercises-dialog.component.ts
+++ src/app/modals/manage-exercises-dialog/manage-exercises-dialog.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material';
+import { ExerciseService } from '../../services/exercise.service';
+import { BodyPart } from '../../../interfaces/bodyPart';
+import { Exercise } from '../../../interfaces/exercise';
+import { UserData } from '../../../interfaces/UserData';
 
 @Component({
   selector: 'app-manage-exercises-dialog',
@@ -9,14 +13,20 @@ import { MatDialogRef } from '@angular/material';
 })
 export class ManageExercisesDialogComponent implements OnInit {
 
+  userData: UserData;
+
   constructor(
     private fb: FormBuilder,
+    private exerciseService: ExerciseService
   ) {
     
   }
 
   ngOnInit() {
-
+    this.exerciseService.getUserData().then((userData: UserData) => {
+      this.userData = userData;
+      console.log(this.userData);
+    });
   }
 
 }
