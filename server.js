1df57cdaee7181ee3eddb2411853aa430bd3c9d6
Shane Hobson <shanehobson1@gmail.com>
2019-06-24 19:54:45 -0700

    Edits to npm scripts

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 8080;
 const host = '0.0.0.0';
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/workout-tracker'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/workout-tracker/index.html'));
 });
 
 app.listen(port, host);
