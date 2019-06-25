48c268ae4d0302c397aa18954abe255cd497fe3c
Shane Hobson <shanehobson1@gmail.com>
2019-06-24 18:57:15 -0700

    Updates to server.js for deployment

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const app = express();
 
+const port = process.env.PORT || 8080;
+const host = '0.0.0.0';
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
@@ -12,5 +15,4 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port, host);
